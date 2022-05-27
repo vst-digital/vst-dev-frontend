f3ccf3fedf05d7a879450add44c091ee739db886
Deran Randell <deranrandell@gmail.com>
2022-05-27 22:51:16 +0200

    added folder/file creation and upload

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,8 @@ Chart.helpers.extend(Chart.elements.Rectangle.prototype, {draw: draw});
 
 const App = () => {
     const history = useHistory();
-    // const isAuthenticated = useSelector(store => store.auth.isAuthenticated);
-    const isAuthenticated = "true"
+    const isAuthenticated = useSelector(store => store.auth.isAuthenticated);
+    // const isAuthenticated = "true"
     const isLoading = useSelector(store => store.auth.loading);
     const notification = useSelector(store => store.config.notification);
     const dispatch = useDispatch();
